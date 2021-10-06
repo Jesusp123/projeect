1bfb7ee2c8f8165e07243b135d8f01eedb537aaf
Jesusp123 <87102287+Jesusp123@users.noreply.github.com>
2021-10-06 01:34:09 -0700

    Create main3.java

---
+++ main3.java
@@ -0,0 +1,12 @@
+package work3;
+
+public class main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		anb out = new anb();	
+		anb.inside in =out.new inside();
+		System.out.println(out.x + in.y);
+}
+}
+
